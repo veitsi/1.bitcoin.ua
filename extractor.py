ccbd5a604051c5c5f816576499ce906902cccdb4
pydev <xtfkpi@gmail.com>
2016-09-24 20:56:20 +0300

    it takes data from kuna server

---
+++ extractor.py
@@ -0,0 +1,9 @@
+import urllib.request
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+url ="https://kuna.io/api/v2/tickers/btcuah"
+headers={'User-Agent':user_agent}
+
+request=urllib.request.Request(url,None,headers) #The assembled request
+response = urllib.request.urlopen(request)
+data = response.read() # The data u need
+print (data)
